03f0539ab145b495ea871ff0ea182f8745533bb1
VaishnaviChin <70666818+VaishnaviChin@users.noreply.github.com>
2021-01-17 13:46:33 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var path,mainCyclist;
+var player1,player2,player3;
+var pathImg,mainRacerImg1,mainRacerImg2;
+
+var oppPink1Img,oppPink2Img;
+var oppYellow1Img,oppYellow2Img;
+var oppRed1Img,oppRed2Img;
+var gameOverImg,cycleBell;
+
+var pinkCG, yellowCG,redCG; 
+
+var END =0;
+var PLAY =1;
+var gameState = PLAY;
+
+var distance=0;
+var gameOver, restart;
+
+function preload(){
+  pathImg = loadImage("images/Road.png");
+  mainRacerImg1 = loadAnimation("images/mainPlayer1.png","images/mainPlayer2.png");
+  mainRacerImg2= loadAnimation("images/mainPlayer3.png");
+  
+  oppPink1Img = loadAnimation("images/opponent1.png","images/opponent2.png");
+  oppPink2Img = loadAnimation("images/opponent3.png");
+  
+  oppYellow1Img = loadAnimation("images/opponent4.png","images/opponent5.png");
+  oppYellow2Img = loadAnimation("images/opponent6.png");
+  
+  oppRed1Img = loadAnimation("images/opponent7.png","images/opponent8.png");
+  oppRed2Img = loadAnimation("images/opponent9.png");
+  
+  cycleBell = loadSound("sound/bell.mp3");
+  gameOverImg = loadImage("images/gameOver.png");
+}
+
+function setup(){
+  
+createCanvas(1200,300);
+// Moving background
+path=createSprite(100,150);
+path.addImage(pathImg);
+path.velocityX = -5;
+
+//creating boy running
+mainCyclist  = createSprite(70,150);
+mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+mainCyclist.scale=0.07;
+  
+gameOver = createSprite(650,150);
+gameOver.addImage(gameOverImg);
+gameOver.scale = 0.8;
+gameOver.visible = false;  
+  
+pinkCG = new Group();
+yellowCG = new Group();
+redCG = new Group();
+  
+}
+
+function draw() {
+  background(0);
+  
+  drawSprites();
+}
+  
+  
